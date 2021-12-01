cdb377f38780af33bed4faa810763627522b67d5
nopnop2002 <hau26280@tree.odn.ne.jp>
2021-12-01 09:09:19 +0900

    Changed from tcpip_adapter_init to esp_netif_init

--- main/ds1302.c
+++ main/ds1302.c
@@ -29,9 +29,12 @@
  */
 bool DS1302_begin(DS1302_Dev *dev, uint8_t clkPin, uint8_t ioPin, uint8_t cePin)
 {
-    gpio_pad_select_gpio(clkPin);
-    gpio_pad_select_gpio(ioPin);
-    gpio_pad_select_gpio(cePin);
+    //gpio_pad_select_gpio(clkPin);
+    //gpio_pad_select_gpio(ioPin);
+    //gpio_pad_select_gpio(cePin);
+    gpio_reset_pin(clkPin);
+    gpio_reset_pin(ioPin);
+    gpio_reset_pin(cePin);
 
     dev->clkPin = clkPin;
     dev->ioPin = ioPin;
